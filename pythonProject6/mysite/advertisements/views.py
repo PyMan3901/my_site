bb463d155275d69d38e108348baab5fcca801e5e
PyMan3901 <126311873+PyMan3901@users.noreply.github.com>
2023-07-12 11:52:15 +0500

    initial

---
+++ pythonProject6/mysite/advertisements/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(requests):
+    return HttpResponse('Все прошло замечательно :)')
+
+
+
+
+
+
+# Create your views here.
